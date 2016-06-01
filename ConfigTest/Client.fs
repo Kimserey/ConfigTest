f13a7f1426134de7e4a05b0a454d6303082c1ac2
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-06-01 14:21:23 +0100

    Add project files.

---
+++ ConfigTest/Client.fs
@@ -0,0 +1,23 @@
+namespace ConfigTest
+
+open WebSharper
+open WebSharper.UI.Next
+open WebSharper.UI.Next.Client
+open WebSharper.UI.Next.Html
+open WebSharper.JavaScript
+
+[<JavaScript>]
+module Client =
+
+    [<Inline "Config">]
+    let config = X<obj>
+
+    let Main =
+        let hello = config?hello
+        let test = config?test
+
+        div [
+            text hello
+            text test
+        ]
+        |> Doc.RunById "main"
